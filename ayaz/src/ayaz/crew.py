d0eba294d0ca0629b956ff3d99d8d055fb7d601a
umiar khan <asadsher2324@gmail.com>
2025-02-15 00:18:57 +0500

    updated crew.py

--- ayaz/src/ayaz/crew.py
+++ ayaz/src/ayaz/crew.py
@@ -7,7 +7,7 @@ from crewai.project import CrewBase, agent, crew, task
 
 @CrewBase
 class Ayaz():
-	"""Ayaz crew"""
+	"""Ayaz crew for handling various workflow tasks through a supervisor-based approach"""
 
 	# Learn more about YAML configuration files here:
 	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
@@ -18,16 +18,37 @@ class Ayaz():
 	# If you would like to add tools to your agents, you can learn more about it here:
 	# https://docs.crewai.com/concepts/agents#agent-tools
 	@agent
-	def researcher(self) -> Agent:
+	def supervisor_agent(self) -> Agent:
 		return Agent(
-			config=self.agents_config['researcher'],
+			config=self.agents_config['Supervisor_agent'],
 			verbose=True
 		)
 
 	@agent
-	def reporting_analyst(self) -> Agent:
+	def email_processor(self) -> Agent:
 		return Agent(
-			config=self.agents_config['reporting_analyst'],
+			config=self.agents_config['email_processor'],
+			verbose=True
+		)
+
+	@agent
+	def calendar_manager(self) -> Agent:
+		return Agent(
+			config=self.agents_config['calendar_manager'],
+			verbose=True
+		)
+
+	@agent
+	def document_manager(self) -> Agent:
+		return Agent(
+			config=self.agents_config['document_manager'],
+			verbose=True
+		)
+
+	@agent
+	def spreadsheet_manager(self) -> Agent:
+		return Agent(
+			config=self.agents_config['spreadsheet_manager'],
 			verbose=True
 		)
 
@@ -35,21 +56,38 @@ class Ayaz():
 	# task dependencies, and task callbacks, check out the documentation:
 	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
 	@task
-	def research_task(self) -> Task:
+	def orchestrate_workflow(self) -> Task:
 		return Task(
-			config=self.tasks_config['research_task'],
+			config=self.tasks_config['orchestrate_workflow'],
 		)
 
 	@task
-	def reporting_task(self) -> Task:
+	def process_email(self) -> Task:
 		return Task(
-			config=self.tasks_config['reporting_task'],
-			output_file='report.md'
+			config=self.tasks_config['process_email'],
+		)
+
+	@task
+	def schedule_meeting(self) -> Task:
+		return Task(
+			config=self.tasks_config['schedule_meeting'],
+		)
+
+	@task
+	def generate_document(self) -> Task:
+		return Task(
+			config=self.tasks_config['generate_document'],
+		)
+
+	@task
+	def update_spreadsheet(self) -> Task:
+		return Task(
+			config=self.tasks_config['update_spreadsheet'],
 		)
 
 	@crew
 	def crew(self) -> Crew:
-		"""Creates the Ayaz crew"""
+		"""Creates the Ayaz crew with a supervisor-based workflow"""
 		# To learn how to add knowledge sources to your crew, check out the documentation:
 		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
